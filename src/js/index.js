c17f86f9a0c64ee9b1ba4a6197d759caa07a4f26
Roman Dilmi <roman.dilmi@orange.fr>
2018-07-17 02:48:04 +0200

    deleting useless files

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,6 @@
 // three.js
-import * as THREE from 'three';
 
 // gsap
 import 'gsap';
 
-//import './base.js';
-//import './fps2.js';
-//import './picking.js';
-//import './picking_anim.js';
 import './minecraft.js';
